8fd0519540cddb83047a5dd06721b51c89fe3d9c
SyedHammad2000 <Hammadimran736@gmail.com>
2024-08-31 21:22:53 +0500

    new change

--- src/helper/baseURL.js
+++ src/helper/baseURL.js
@@ -1,5 +1,5 @@
 const baseURL =process.env.NODE_ENV === "production"
-    ? "https://job-portal-davj.vercel.app"
+    ? "https://dynamic-sunflower-844e8b.netlify.app"
     : "http://localhost:3000";
 
 export default baseURL;
