a0d0ceeccaf6ef1e915055e6ff0a67fa8ef6a4f8
SyedHammadgit <116845584+SyedHammad2000@users.noreply.github.com>
2024-08-31 18:04:42 +0500

    Update login.js

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -4,6 +4,12 @@ import Usermodel from "@/utils/models/Usermodel";
 import jwt from "jsonwebtoken";
 
 export default async (req, res) => {
+   res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://job-portal-chi-taupe.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   await ConnectDb();
   //! get method
   // if (req.method !== "POST") {
