d7b8d1807eb5e77347126c7247e8b96e5f6b2bf4
SyedHammad2000 <Hammadimran736@gmail.com>
2024-08-31 19:35:45 +0500

    changes

--- src/helper/baseURL.js
+++ src/helper/baseURL.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://job-portal-2024.vercel.app"
+    ? "https://job-portal-davj.vercel.app/"
     : "http://localhost:3000";
 
 export default baseURL;
