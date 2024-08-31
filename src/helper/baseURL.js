1e67308a88265c7d70e8da83033d6aceae3c26c2
SyedHammad2000 <Hammadimran736@gmail.com>
2024-08-31 21:29:02 +0500

    changes

--- src/helper/baseURL.js
+++ src/helper/baseURL.js
@@ -1,5 +1,6 @@
-const baseURL =process.env.NODE_ENV === "production"
-    ? "https://dynamic-sunflower-844e8b.netlify.app"
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://job-portal-management.netlify.app"
     : "http://localhost:3000";
 
 export default baseURL;
