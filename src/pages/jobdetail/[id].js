0c849b92dc4bbd9dda3ac87ea677d84e4327d075
SyedHammad2000 <Hammadimran736@gmail.com>
2024-09-03 14:50:54 +0500

    chnages

--- src/pages/jobdetail/[id].js
+++ src/pages/jobdetail/[id].js
@@ -14,6 +14,10 @@ import React from "react";
 
 const Jobdetail = ({ data }) => {
   const { post } = data;
+  const pic =
+    " https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+  const matchpic = pic === post.postedBy.pic;
+
   let postdate = post.createdAt.slice(0, 10);
   console.log(post);
   return (
@@ -24,6 +28,7 @@ const Jobdetail = ({ data }) => {
       p={5}
       justifyContent={"center"}
       alignItems={"center"}
+      background={"linear-gradient(40deg,blue,lightblue,black)"}
     >
       <Box
         height={"80%"}
@@ -31,30 +36,36 @@ const Jobdetail = ({ data }) => {
         boxShadow={"4px 2px 7px"}
         rounded={"md"}
         p={5}
+        bg={"lightblue"}
       >
         <Text fontFamily={"cursive"} fontSize={"1.2rem"}>
-          <img
-            ml={"10px"}
-            className="rounded-full h-[40px] w-[45px] inline mr-2"
-            src={post.postedBy.pic}
-          />{" "}
+          {matchpic ? (
+            <Avatar size={"sm"} name={post.postedBy.name} mr={"10px"} />
+          ) : (
+            <img
+              ml={"10px"}
+              className="rounded-full h-[40px] w-[45px] inline mr-2"
+              src={post.postedBy.pic}
+            />
+          )}
           HR {post.postedBy.name}
         </Text>
         <Box
           position={"absolute"}
           top="21rem"
-          width={["80%", "40%", "45%", "45%"]}
+          width={["80%", "50%", "45%", "45%"]}
           flexWrap={"wrap"}
           fontSize={["12px", "md", "md", "lg"]}
-          fontWeight={["bold", "normal"]}
+          fontWeight={["bold", "bold"]}
         >
           <Text size={"md"}>Job Title: {post.title}</Text>
           <Text>Company: {post.company}</Text>
           <Text>Location: {post.location}</Text>
           Job Requirement:
-          <Text maxHeight={"100px"} overflowY={"auto"}>
+          <Text maxHeight={"50px"} overflowY={"auto"} px={2}>
             {post.description}
           </Text>
+          <Text>Email me for query {post.postedBy.email}</Text>
           <Text>Posted on: {postdate}</Text>
         </Box>
       </Box>
