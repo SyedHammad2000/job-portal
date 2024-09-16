851cfaf0a4bac264094f99a8a0b04d8b0da97214
SyedHammad2000 <Hammadimran736@gmail.com>
2024-09-17 04:32:43 +0500

    changes

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -1,7 +1,17 @@
 // pages/api/socket.js
 import { initSocket } from "@/utils/socket/initSocket";
+import NextCors from "nextjs-cors";
 
-export default (req, res) => {
+export default async (req, res) => {
+  // ! cors
+
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "https://job-portal-management.netlify.app/",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   if (res.socket.server.io) {
     console.log("Socket.IO already initialized");
     return res.end();
